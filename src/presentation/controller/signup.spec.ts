eaac66c9ecbc7e20aea0ea61983d9a62a8f11010
Cleriston <cleriston.mari@gmail.com>
2021-07-24 00:56:07 +0100

    test: ensure SignUpController return error if name not provided

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from './signUpController'
 
 describe('Signup  Controller', () => {
-  test('Should return 400 if no name is provided', () => {
+  test('Should return 400 if name not provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       email: 'cleriston.mari@gmail.com',
@@ -10,5 +10,6 @@ describe('Signup  Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing name'))
   })
 })
