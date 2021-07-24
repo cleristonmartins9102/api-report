c0403f02d1025c27784e60c9fd5e76b643dc61cb
Cleriston <cleriston.mari@gmail.com>
2021-07-24 14:44:00 +0100

    refactor: ensure SignUpController.handle implements the correct interfaces

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -15,17 +15,17 @@ describe('Signup  Controller', () => {
     expect(httpResponse.body).toEqual(new Error('Missing name'))
   })
 
-  // test('Should return 400 if email not provided', () => {
-  //   const sut = new SignUpController()
-  //   const httpRequest = {
-  //     body: {
-  //       name: 'cleriston',
-  //       email: 'cleriston.mari@gmail.com',
-  //       password: 'any_password',
-  //       passwordConfirm: 'any_password'
-  //     }
-  //   }
-  //   const httpResponse = sut.handle(httpRequest)
-  //   expect(httpResponse.body).toEqual(new Error('Missing Email'))
-  // })
+  test('Should return 400 if email not provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'cleriston',
+        password: 'any_password',
+        passwordConfirm: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing Email'))
+  })
 })
---
+++ src/presentation/protocols/http.ts
@@ -0,0 +1,8 @@
+export class HttpResponse {
+  statusCode: number
+  body: any
+}
+
+export class HttpRequest {
+  body?: any
+}
