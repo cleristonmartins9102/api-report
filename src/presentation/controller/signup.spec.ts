cd4f057305ba7d3ce15d950a498dda156bc3ee09
Cleriston <cleriston.mari@gmail.com>
2021-07-24 01:39:00 +0100

    test: ensure return 400 if email not provided

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -4,12 +4,28 @@ describe('Signup  Controller', () => {
   test('Should return 400 if name not provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
-      email: 'cleriston.mari@gmail.com',
-      password: 'any_password',
-      passwordConfirm: 'any_password'
+      body: {
+        email: 'cleriston.mari@gmail.com',
+        password: 'any_password',
+        passwordConfirm: 'any_password'
+      }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing name'))
   })
+
+  // test('Should return 400 if email not provided', () => {
+  //   const sut = new SignUpController()
+  //   const httpRequest = {
+  //     body: {
+  //       name: 'cleriston',
+  //       email: 'cleriston.mari@gmail.com',
+  //       password: 'any_password',
+  //       passwordConfirm: 'any_password'
+  //     }
+  //   }
+  //   const httpResponse = sut.handle(httpRequest)
+  //   expect(httpResponse.body).toEqual(new Error('Missing Email'))
+  // })
 })
