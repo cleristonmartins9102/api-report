8cc4228a7a6baa7b32eaa54a1049e1e77af16255
Cleriston <cleriston.mari@gmail.com>
2021-07-28 00:45:04 +0100

    refactor: move sut to factor helper method

--- src/presentation/uteis/email-validator-adapter.spec.ts
+++ src/presentation/uteis/email-validator-adapter.spec.ts
@@ -1,5 +1,6 @@
-import { EmailValidatorAdapter } from './email-validator'
+import { EmailValidatorAdapter } from './email-validator-adapter'
 import validator from 'validator'
+import { EmailValidator } from '../protocols/email-validator'
 
 jest.mock('validator', () => ({
   isEmail: (): boolean => {
@@ -7,23 +8,34 @@ jest.mock('validator', () => ({
   }
 }))
 
+type SutType = {
+  sut: EmailValidator
+}
+
+const makeSut = (): SutType => {
+  const sut = new EmailValidatorAdapter()
+  return {
+    sut
+  }
+}
+
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const { sut } = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@gmail.com')
     expect(isValid).toBe(false)
   })
 
   test('Should return true if validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const { sut } = makeSut()
     const isValid = sut.isValid('cleriston.mari@gmail.com')
 
     expect(isValid).toBe(true)
   })
 
   test('Should validator receive correct email', () => {
-    const sut = new EmailValidatorAdapter()
+    const { sut } = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     const email = 'cleriston.mari@gmail.com'
     sut.isValid(email)
