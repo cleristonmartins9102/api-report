c5379b6ec787fdea058a1e141237f432d745e164
Cleriston <cleriston.mari@gmail.com>
2021-07-30 00:49:00 +0100

    feat: ensure Bcrypt returns hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,13 @@
 import { BcryptAdapter } from './bcrypt-adapter'
 import bcrypt from 'bcrypt'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('any_password'))
+  }
+})
+)
+
 describe('Bcrypt Adapter', () => {
   test('Shold call bcrypt with correct value', async () => {
     const salt = 12
@@ -9,5 +16,14 @@ describe('Bcrypt Adapter', () => {
     const password = 'valid_password'
     await sut.encrypt(password)
     expect(encryptSpy).toHaveBeenCalledWith(password, 12)
+  })
+
+  test('Should return hash is on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const encryptSpy = jest.spyOn(bcrypt, 'hash')
+    const password = 'any_password'
+    const hash = await sut.encrypt(password)
+    expect(hash).toBe(password)
   })
 })
