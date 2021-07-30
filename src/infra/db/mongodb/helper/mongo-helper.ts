c57a8996734d6df990a6fdb7249f1774fc4f85db
Cleriston <cleriston.mari@gmail.com>
2021-07-30 13:43:21 +0100

    feat: ensure AddAccountRepository returns account on success

--- src/infra/db/mongodb/helper/mongo-helper.ts
+++ src/infra/db/mongodb/helper/mongo-helper.ts
@@ -1,15 +1,22 @@
-import { Db, MongoClient } from 'mongodb'
+import { Collection, Db, MongoClient } from 'mongodb'
 
-export class MongoHelper {
-  private connection: MongoClient
-  private db: Db
+export const mongoHelper = {
+  connection: MongoClient,
+  db: Db,
 
   async connect (): Promise<any> {
-    this.connection = await MongoClient.connect(process.env.MONGO_URL as any)
+    this.connection = await MongoClient.connect(process.env.MONGO_URL as string, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    } as any)
     this.db = await this.connection.db()
-  }
+  },
 
   async close (): Promise<any> {
     await this.connection.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.connection.db().collection(name)
   }
 }
