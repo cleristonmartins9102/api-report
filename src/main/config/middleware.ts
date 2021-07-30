6764f3414b9fee100adfe1aac6d32159ecd1d8f8
Cleriston <cleriston.mari@gmail.com>
2021-07-30 17:37:54 +0100

    feat: ensure requests return json as default on content type

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -2,8 +2,10 @@ import app from '../config/app'
 import { Express } from 'express'
 import { bodyParser } from '../midlewares/body-parser'
 import { cors } from '../midlewares/cors'
+import { contentType } from '../midlewares/content-type'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
