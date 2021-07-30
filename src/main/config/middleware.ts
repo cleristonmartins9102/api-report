491059837216d10ea8975c962b2063c0932ad07e
Cleriston <cleriston.mari@gmail.com>
2021-07-30 17:21:03 +0100

    feat: ensure cors is enabled

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,7 +1,9 @@
 import app from '../config/app'
 import { Express } from 'express'
 import { bodyParser } from '../midlewares/body-parser'
+import { cors } from '../midlewares/cors'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
