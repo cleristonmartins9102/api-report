351b09df7cea10de67524414fc4f442071489e3b
Cleriston <cleriston.mari@gmail.com>
2021-07-30 18:27:36 +0100

    feat: ensure SignUp route return an account on success

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,8 +1,6 @@
 import app from '../config/app'
 import { Express } from 'express'
-import { bodyParser } from '../midlewares/body-parser'
-import { cors } from '../midlewares/cors'
-import { contentType } from '../midlewares/content-type'
+import { bodyParser, cors, contentType } from '../midlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
