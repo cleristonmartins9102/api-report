0415c0c8c51571ad9882ddba00f2686900b1b78d
Cleriston <cleriston.mari@gmail.com>
2021-09-03 23:29:16 +0100

    refactor: move creator to a factory method

--- src/infra/db/mongodb/log.spec.ts
+++ src/infra/db/mongodb/log.spec.ts
@@ -2,6 +2,10 @@ import { mongoHelper } from './helper/mongo-helper'
 import { Collection } from 'mongodb'
 import { LogMongoRepository } from './log'
 
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
+
 describe('Log Mongo DB', () => {
   let errorCollection: Collection
   beforeAll(async () => {
@@ -18,7 +22,7 @@ describe('Log Mongo DB', () => {
   })
 
   test('Should create an error log on success', async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError('any_error')
     const count = await errorCollection.countDocuments()
     expect(count).toBe(1)
