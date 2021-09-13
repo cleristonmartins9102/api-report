a6a7362f44dd120073eaa93ca9d5a52ca5cb87c3
Cleriston <cleriston.mari@gmail.com>
2021-09-13 10:14:26 +0100

    test: ensure DbAuthentication returns throw if LoadAccountByEmailRepository throw

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -2,28 +2,47 @@ import { LoadAuthenticationByEmailRepository } from '../../protocols/load-authen
 import { AccountModel } from '../add-account/db-account-protocols'
 import { DbAuthentication } from './db-authentication'
 
+const makeFakeAccount = (): AccountModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  password: 'any_password',
+  email: 'cleriston.mari@gmail.com'
+})
+
+const makeLoadAuthenticationByEmailStub = (): LoadAuthenticationByEmailRepository => {
+  class LoadAccountByEmailRepositoryStub implements LoadAuthenticationByEmailRepository {
+    async load (email: string): Promise<AccountModel> {
+      const account: AccountModel = makeFakeAccount()
+      return new Promise(resolve => resolve(account))
+    }
+  }
+  return new LoadAccountByEmailRepositoryStub()
+}
+
 describe('DBAuthentication UseCase', () => {
   test('Should call LoadAccountByEmailRepository with correct email', async () => {
-    class LoadAccountByEmailRepositoryStub implements LoadAuthenticationByEmailRepository {
-      async load (email: string): Promise<AccountModel> {
-        const account: AccountModel = {
-          id: 'any_id',
-          name: 'any_name',
-          password: 'any_password',
-          email: 'cleriston.mari@gmail.com'
-        }
-        return new Promise(resolve => resolve(account))
-      }
-    }
-    const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
+    const { email, password } = makeFakeAccount()
+    const loadAccountByEmailRepositoryStub = makeLoadAuthenticationByEmailStub()
     const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
     const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
     await sut.auth(
-      'cleriston.mari@gmail.com',
-      'sw12digit'
+      email,
+      password
     )
     expect(loadSpy).toHaveBeenCalledWith(
-      'cleriston.mari@gmail.com'
+      email
     )
+  })
+
+  test('Should throw if LoadAccountByEmailRepository throw', async () => {
+    const { email, password } = makeFakeAccount()
+    const loadAccountByEmailRepositoryStub = makeLoadAuthenticationByEmailStub()
+    const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockResolvedValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.auth(
+      email,
+      password
+    )
+    expect(promise).rejects.toThrow()
   })
 })
