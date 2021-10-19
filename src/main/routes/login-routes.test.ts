1967111fdfff2bb99c016b559802d7348702cdb7
Cleriston <cleriston.mari@gmail.com>
2021-10-19 10:05:54 +0100

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -54,5 +54,17 @@ describe('Login Routes', () => {
         )
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send(
+          {
+            email: 'valid_email@gmail.com',
+            password: 'valid_password'
+          }
+        )
+        .expect(401)
+    })
   })
 })
