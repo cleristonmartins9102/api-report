5b9b19f67ca7668a354401a6822a075630c566e5
Martins Cleriston <cleriston.mari@gmail.com>
2022-04-08 12:39:17 +0100

    feat: ensure returns 500 if AddSurvey throws

--- src/presentation/controller/survey/add-servey/add-survey-controller.ts
+++ src/presentation/controller/survey/add-servey/add-survey-controller.ts
@@ -1,25 +1,29 @@
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from './add-survey-controller-protocols'
 import { Validation } from '../../../protocols/validations'
-import { badRequest } from '../../../helpers/http/http-helpers'
+import { badRequest, serverError } from '../../../helpers/http/http-helpers'
 import { AddSurvey } from '../../../../domain/usercases/add-survey'
 
 export class AddSurveyController implements Controller {
   constructor (
     private readonly validation: Validation,
     private readonly addSurvey: AddSurvey
-  ) {}
+  ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse | any> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { question, answers } = httpRequest.body
+      await this.addSurvey.add({
+        question,
+        answers
+      })
+      return null
+    } catch (error) {
+      return serverError(error)
     }
-    const { question, answers } = httpRequest.body
-    await this.addSurvey.add({
-      question,
-      answers
-    })
-    return null
   }
 }
