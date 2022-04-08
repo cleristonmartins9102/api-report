cde0584601eb209d88bb7701fbd698f277d3a2dc
Martins Cleriston <cleriston.mari@gmail.com>
2022-04-08 11:49:32 +0100

    feat: ensure AddSurveyController calls AddSurvey with correct values

---
+++ src/domain/usercases/add-survey.ts
@@ -0,0 +1,13 @@
+interface AddSurveyModel {
+  question: string
+  answers: SurveyAnswer[]
+}
+
+interface SurveyAnswer {
+  image: string
+  answer: string
+}
+
+export interface AddSurvey {
+  add (data: AddSurveyModel): Promise<void>
+}
--- src/presentation/controller/survey/add-servey/add-survey-controller.ts
+++ src/presentation/controller/survey/add-servey/add-survey-controller.ts
@@ -1,19 +1,25 @@
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from './add-survey-controller-protocols'
 import { Validation } from '../../../protocols/validations'
+import { badRequest } from '../../../helpers/http/http-helpers'
+import { AddSurvey } from '../../../../domain/usercases/add-survey'
 
 export class AddSurveyController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
   ) {}
 
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
-    return new Promise(resolve => {
-      resolve({
-        statusCode: 1,
-        body: 'any_body'
-      })
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse | any> {
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return badRequest(error)
+    }
+    const { question, answers } = httpRequest.body
+    await this.addSurvey.add({
+      question,
+      answers
     })
+    return null
   }
 }
