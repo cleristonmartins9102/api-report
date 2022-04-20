fbef3d1944ae5b309a6ccb8be8a98b5f67df4491
Martins Cleriston <cleriston.mari@gmail.com>
2022-04-20 09:21:56 +0100

    test: ensure call DbSaveSurveyResult throws if SaveSurveyRepository throws

--- src/data/usecases/survey/save-survey-result/db-save-survey-result.spec.ts
+++ src/data/usecases/survey/save-survey-result/db-save-survey-result.spec.ts
@@ -2,6 +2,7 @@ import { SurveyResultModel } from '../../../../domain/model/survey-result-model'
 import { SaveSurveyResult, SaveSurveyResultModel } from '../../../../domain/usercases/save-survey-result'
 import { SaveSurveyResultRepository } from '../../../protocols/db/survey'
 import { DbSaveSurveyResult } from './db-save-survey-result'
+import MockDate from 'mockdate'
 
 type SutTypes = {
   sut: SaveSurveyResult
@@ -43,6 +44,14 @@ const makeSaveSurveyResultModel = (): SaveSurveyResultModel => (
   }
 )
 
+beforeAll(() => {
+  MockDate.set(new Date())
+})
+
+afterAll(() => {
+  MockDate.reset()
+})
+
 describe('Save Survey', () => {
   test('Shoud call SaveSurveyResultRepository with correct value', async () => {
     const { sut, saveSurveyRepositoryStub } = makeSut()
@@ -53,8 +62,8 @@ describe('Save Survey', () => {
 
   test('Shoud throw if SaveSurveyRepository throws', async () => {
     const { sut, saveSurveyRepositoryStub } = makeSut()
-    jest.spyOn(saveSurveyRepositoryStub, 'save')
-    await sut.save(makeSaveSurveyResultModel())
-    expect(saveSurveyRepoSpy).toBeCalledWith(makeSaveSurveyResultModel())
+    jest.spyOn(saveSurveyRepositoryStub, 'save').mockReturnValueOnce(Promise.reject(new Error()))
+    const response = sut.save(makeSaveSurveyResultModel())
+    await expect(response).rejects.toThrow(new Error())
   })
 })
