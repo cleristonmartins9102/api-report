62fbde27f9b7b002fff4f78deba8840a8dd214b9
Martins Cleriston <cleriston.mari@gmail.com>
2022-04-27 13:25:31 +0100

    feat: ensure DbLoadSurveyResult returns 200 on success

--- src/presentation/controller/survey/survey-result/load/load-surver-result-controller.ts
+++ src/presentation/controller/survey/survey-result/load/load-surver-result-controller.ts
@@ -1,4 +1,4 @@
-import { serverError } from '../../../../helpers/http/http-helpers'
+import { noContent, ok, serverError } from '../../../../helpers/http/http-helpers'
 import { Controller, HttpRequest, HttpResponse, LoadSurveyResult } from './load-survey-result-controller-protocols'
 
 export class LoadSurveyResultController implements Controller {
@@ -11,8 +11,12 @@ export class LoadSurveyResultController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { params } = httpRequest
-      await this.dbLoadSurveyResult.load(params.surveyId)
-      return null as any
+      const surveyResult = await this.dbLoadSurveyResult.load(params.surveyId)
+      if (surveyResult) {
+        return ok(surveyResult)
+      } else {
+        return noContent()
+      }
     } catch (error) {
       return serverError(error)
     }
